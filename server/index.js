5647c8c176bc8b263b3f38dc9a5091be4c7b88ea
Shivakant <kants6397@gmail.com>
2024-03-01 21:19:31 +0530

    server started

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express=require('express')
+
+const app = express();
+const port=5000
+
+
+app.get('/', (req, res) => {
+    const message = 'Hi there !'
+    res.json({message})
+})
+app.listen(port,() => {
+    console.log(`Server is running at http://localhost:${port}`);
+})
